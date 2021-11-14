c91b83171d2bb6e103f8194f5a10993d5df9f22b
winterfoxxy <winterarctic26@outlook.com>
2021-11-14 16:28:33 -0500

    Add project files.

---
+++ NTools2/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace NTools2
+{
+    partial class NTools
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Button();
+            this.efi = new System.Windows.Forms.Button();
+            this.onedriveremover = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.White;
+            this.title.Location = new System.Drawing.Point(13, 13);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(123, 43);
+            this.title.TabIndex = 0;
+            this.title.Text = "NTools";
+            this.title.Click += new System.EventHandler(this.title_Click);
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.exit.FlatAppearance.BorderSize = 0;
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit.ForeColor = System.Drawing.Color.White;
+            this.exit.Location = new System.Drawing.Point(752, 13);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(24, 26);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "X";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // efi
+            // 
+            this.efi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.efi.FlatAppearance.BorderSize = 0;
+            this.efi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.efi.Font = new System.Drawing.Font("Microsoft YaHei Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.efi.ForeColor = System.Drawing.Color.White;
+            this.efi.Location = new System.Drawing.Point(553, 80);
+            this.efi.Name = "efi";
+            this.efi.Size = new System.Drawing.Size(127, 46);
+            this.efi.TabIndex = 2;
+            this.efi.Text = "Mount EFI Partition";
+            this.efi.UseVisualStyleBackColor = false;
+            this.efi.Click += new System.EventHandler(this.efi_Click);
+            // 
+            // onedriveremover
+            // 
+            this.onedriveremover.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.onedriveremover.FlatAppearance.BorderSize = 0;
+            this.onedriveremover.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.onedriveremover.Font = new System.Drawing.Font("Microsoft YaHei Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onedriveremover.ForeColor = System.Drawing.Color.White;
+            this.onedriveremover.Location = new System.Drawing.Point(287, 80);
+            this.onedriveremover.Name = "onedriveremover";
+            this.onedriveremover.Size = new System.Drawing.Size(127, 46);
+            this.onedriveremover.TabIndex = 3;
+            this.onedriveremover.Text = "Remove OneDrive";
+            this.onedriveremover.UseVisualStyleBackColor = false;
+            this.onedriveremover.Click += new System.EventHandler(this.onedriveremover_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.progressBar1.ForeColor = System.Drawing.Color.Blue;
+            this.progressBar1.Location = new System.Drawing.Point(21, 64);
+            this.progressBar1.MarqueeAnimationSpeed = 500;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(755, 10);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(21, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clean Disk";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(154, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Verify System File Integrity";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei Light", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(128, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "beta";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(420, 80);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 46);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Remove Cortana";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // NTools
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.onedriveremover);
+            this.Controls.Add(this.efi);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NTools";
+            this.Text = "NTools";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button efi;
+        private System.Windows.Forms.Button onedriveremover;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ NTools2/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NTools2
+{
+    public partial class NTools : Form
+    {
+
+        public Font quicksand;
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont,
+           IntPtr pdv, [System.Runtime.InteropServices.In] ref uint pcFonts);
+
+        private PrivateFontCollection fonts = new PrivateFontCollection();
+        public NTools()
+        {
+            InitializeComponent();
+            byte[] fontData = Properties.Resources.quicksand;
+            IntPtr fontPtr = System.Runtime.InteropServices.Marshal.AllocCoTaskMem(fontData.Length);
+            System.Runtime.InteropServices.Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+            uint dummy = 0;
+            fonts.AddMemoryFont(fontPtr, Properties.Resources.quicksand.Length);
+            AddFontMemResourceEx(fontPtr, (uint)Properties.Resources.quicksand.Length, IntPtr.Zero, ref dummy);
+            System.Runtime.InteropServices.Marshal.FreeCoTaskMem(fontPtr);
+
+            quicksand = new Font(fonts.Families[0], 25.0F);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            title.Font = quicksand;
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void efi_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            string strCmdText;
+            strCmdText = "/C mountvol X: /s";
+            Process p = System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            while (!p.HasExited)
+            {
+
+            }
+            progressBar1.Value = 100;
+        }
+
+        private void onedriveremover_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            string strCmdText;
+            strCmdText = "/C TASKKILL /f /im OneDrive.exe";
+            Process p = System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            while (!p.HasExited)
+            {
+
+            }
+            progressBar1.Value += 50;
+            string getEnv = Environment.GetEnvironmentVariable("systemroot");
+            if (File.Exists(getEnv + "\\System32\\OneDriveSetup.exe"))
+            {
+                strCmdText = "/C %systemroot%\\System32\\OneDriveSetup.exe /uninstall";
+                p = System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+                while (!p.HasExited)
+                {
+
+                }
+            }
+            if (File.Exists(getEnv + "\\SysWOW64\\OneDriveSetup.exe"))
+            {
+                strCmdText = "/C %systemroot%\\SysWOW64\\OneDriveSetup.exe /uninstall";
+                p = System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+                while (!p.HasExited)
+                {
+
+                }
+            }
+            progressBar1.Value = 100;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            string strCmdText;
+            strCmdText = "/C DEL /F /S /Q %TEMP%";
+            Process p = System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            while (!p.HasExited)
+            {
+
+            }
+            progressBar1.Value += 50;
+            strCmdText = "/C cleanmgr.exe /dc";
+            p = System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            while (!p.HasExited)
+            {
+
+            }
+            progressBar1.Value = 100;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            string strCmdText;
+            strCmdText = "/C sfc /scannow";
+            Process p = System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            while (!p.HasExited)
+            {
+
+            }
+            progressBar1.Value += 50;
+            strCmdText = "/C DISM.exe /Online /Cleanup-Image /Restorehealth";
+            p = System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            while (!p.HasExited)
+            {
+
+            }
+            progressBar1.Value = 100;
+        }
+
+        private void title_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            string strCmdText;
+            strCmdText = "/C Powershell -Command \"Get-AppxPackage -allusers Microsoft.549981C3F5F10 | Remove-AppxPackage\"";
+            Process p = System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            while (!p.HasExited)
+            {
+
+            }
+
+            progressBar1.Value = 100;
+        }
+    }
+}
